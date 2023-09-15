cb8ec8269731468f81d349a2f35196940a906a15
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-09-15 14:22:03 +0200

    Setup gem

---
+++ lib/storage/tables.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tables/version"
+
+module Storage
+  module Tables
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
