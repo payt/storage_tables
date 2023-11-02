8ed51e50f60fe00b1ea1d78f1f4cc0fd57dc4f23
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-11-02 20:39:09 +0100

    Remove down migration

--- test/database/setup.rb
+++ test/database/setup.rb
@@ -5,7 +5,5 @@ require_relative "create_storage_tables_user_attachments_migration"
 
 # Writing and reading roles are required for the "previewing on the writer DB" test
 ActiveRecord::Base.connection.migration_context.migrate
-CreateStorageTablesUserAttachmentsMigration.migrate(:down)
-CreateUsersMigration.migrate(:down)
 CreateUsersMigration.migrate(:up)
 CreateStorageTablesUserAttachmentsMigration.migrate(:up)
