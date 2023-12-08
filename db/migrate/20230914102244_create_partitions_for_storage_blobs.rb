94aeba12372c75b9b47c309549c2e11440878f5b
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2023-12-08 12:08:18 +0100

    Cleanup

--- db/migrate/20230914102244_create_partitions_for_storage_blobs.rb
+++ db/migrate/20230914102244_create_partitions_for_storage_blobs.rb
@@ -1,8 +1,8 @@
 class CreatePartitionsForStorageBlobs < ActiveRecord::Migration[7.0]
-  def up    
+  def up
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("").each_with_index do |char, index|
       ActiveRecord::Base.connection.execute <<~SQL.squish
-      CREATE TABLE storage_tables_blobs_partition_#{index} PARTITION OF storage_tables_blobs
+        CREATE TABLE storage_tables_blobs_partition_#{index} PARTITION OF storage_tables_blobs
         FOR VALUES IN ('#{char}');
       SQL
       ActiveRecord::Base.connection.execute <<~SQL.squish
