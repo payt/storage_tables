5d05f6de47e21b16fcd41f09ef3c531583cec12b
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-01-03 15:57:59 +0100

    Add create many class

---
+++ lib/storage_tables/attachable/changes/create_many.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module StorageTables
+    module Attachable
+      module Changes
+        # Class used to create many attachments from an attachable blob.
+        class CreateMany
+            
+        end
+    end
+end
