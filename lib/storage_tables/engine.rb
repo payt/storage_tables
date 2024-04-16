4198d7e3e0ce3f0b6694441e56bfdd37c6b78990
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-04-16 13:56:53 +0200

    Add require "rails/engine" to storage_tables/engine.rb

--- lib/storage_tables/engine.rb
+++ lib/storage_tables/engine.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
+require "rails/engine"
 require "storage_tables"
 require "storage_tables/service/registry"
-
 require "storage_tables/reflection"
 
 module StorageTables
