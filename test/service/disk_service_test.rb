68a98a88bb017a5263ba085cb99d6e456ad54290
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-05-16 10:48:02 +0200

    Fix root path in DiskServiceTest

--- test/service/disk_service_test.rb
+++ test/service/disk_service_test.rb
@@ -7,13 +7,13 @@ module StorageTables
   module Service
     class DiskServiceTest < ActiveSupport::TestCase
       setup do
-        @service = StorageTables::Service::DiskService.new(root: "root_path")
+        @service = StorageTables::Service::DiskService.new(root: "/root_path")
       end
 
       test "path_for" do
         checksum = "1234567890abcde+f=="
 
-        assert_equal "root_path/1/23/45/1234567890abcde-f==", @service.path_for(checksum)
+        assert_equal "/root_path/1/23/45/1234567890abcde-f==", @service.path_for(checksum)
       end
 
       test "relative_path_for" do
