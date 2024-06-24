3c10f84f5749d601b4213c6e4ce442f3bc6f8c6d
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-06-24 11:41:10 +0200

    Added more specs

--- test/controllers/disk_controller_test.rb
+++ test/controllers/disk_controller_test.rb
@@ -16,6 +16,31 @@ module StorageTables
       assert_equal data, blob.download
     end
 
+    test "directly uploading blob with different but equivalent content type" do
+      data = name
+      blob = create_blob_before_direct_upload(
+        byte_size: data.size, checksum: create_checksum(data), content_type: "application/x-gzip"
+      )
+
+      put blob.service_url_for_direct_upload, params: data, headers: { "Content-Type" => "application/x-gzip" }
+
+      assert_response :no_content
+      assert_equal data, blob.download
+    end
+
+    test "upload a pdf" do
+      file = file_fixture("report.pdf")
+      checksum = OpenSSL::Digest.new("SHA3-512").file(file).base64digest
+
+      blob = create_blob_before_direct_upload(byte_size: file.size, checksum:, content_type: "application/pdf")
+
+      put blob.service_url_for_direct_upload, params: file.read, headers: { "Content-Type" => "application/pdf" }
+
+      assert_response :no_content
+      assert blob.service.exist?(blob.checksum)
+      assert_equal checksum, OpenSSL::Digest.new("SHA3-512").base64digest(blob.download)
+    end
+
     test "directly uploading blob without integrity" do
       data = name
       blob = create_blob_before_direct_upload byte_size: data.size,
@@ -37,18 +62,6 @@ module StorageTables
       assert_not blob.service.exist?(blob.checksum)
     end
 
-    test "directly uploading blob with different but equivalent content type" do
-      data = name
-      blob = create_blob_before_direct_upload(
-        byte_size: data.size, checksum: create_checksum(data), content_type: "application/x-gzip"
-      )
-
-      put blob.service_url_for_direct_upload, params: data, headers: { "Content-Type" => "application/x-gzip" }
-
-      assert_response :no_content
-      assert_equal data, blob.download
-    end
-
     test "directly uploading blob with mismatched content length" do
       data = name
       blob = create_blob_before_direct_upload byte_size: data.size - 1,
@@ -65,6 +78,18 @@ module StorageTables
           params: "Something else entirely!", headers: { "Content-Type" => "text/plain" }
 
       assert_response :not_found
+    end
+
+    test "upload with different content" do
+      data = "Hello, world!"
+      mismatched_data = "Hello, world?"
+
+      blob = create_blob_before_direct_upload byte_size: data.size, checksum: create_checksum(data)
+
+      put blob.service_url_for_direct_upload, params: mismatched_data, headers: { "Content-Type" => "text/plain" }
+
+      assert_response :unprocessable_entity
+      assert_not blob.service.exist?(blob.checksum)
     end
 
     test "when uploading fails" do
