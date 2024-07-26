31275473a00ee2559ef11f8d013b6059cca7e4fc
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-07-26 09:15:51 +0200

    Remove active storage reflection so it loses dependecies on this

--- lib/storage_tables/reflection.rb
+++ lib/storage_tables/reflection.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-require "active_storage/reflection"
-
 module StorageTables
   # Storage Tables reflection extensions for Active Record.
   module Reflection
-    include ActiveStorage::Reflection
     # Holds all the metadata about a has_one_attached attachment as it was
     # specified in the Active Record class.
     class StoredOneAttachmentReflection < ActiveRecord::Reflection::MacroReflection # :nodoc:
@@ -18,7 +15,6 @@ module StorageTables
     # specified in the Active Record class.
     class StoredManyAttachmentsReflection < ActiveRecord::Reflection::MacroReflection # :nodoc:
       def macro
-        # TODO: Cover many attachments in other PR
         # :nocov:
         :stored_many_attachments
         # :nocov:
@@ -42,6 +38,31 @@ module StorageTables
         # :nocov:
         else
           super
+        end
+      end
+    end
+
+    module ActiveRecordExtensions
+      extend ActiveSupport::Concern
+
+      included do
+        class_attribute :attachment_reflections, instance_writer: false, default: {}
+      end
+
+      module ClassMethods
+        # Returns an array of reflection objects for all the attachments in the
+        # class.
+        def reflect_on_all_attachments
+          attachment_reflections.values
+        end
+
+        # Returns the reflection object for the named +attachment+.
+        #
+        #    User.reflect_on_attachment(:avatar)
+        #    # => the avatar reflection
+        #
+        def reflect_on_attachment(attachment)
+          attachment_reflections[attachment.to_s]
         end
       end
     end
