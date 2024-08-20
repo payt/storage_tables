6d56c1c16b8a1faf6445c959bbf5a716d162eea7
Mathijs <mathijskingma@gmail.com>
2024-08-20 14:34:51 +0200

    Add support for Rails 7.2

--- test/database/create_users_migration.rb
+++ test/database/create_users_migration.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateUsersMigration < ActiveRecord::Migration[7.1]
+class CreateUsersMigration < ActiveRecord::Migration[7.2]
   def change
     create_table :users, force: :cascade do |t|
       t.string :name
