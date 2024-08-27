24f50dcc5861ac85dcead9265c80b94e6216a66d
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-08-27 08:55:33 +0200

    Update Active Storage service configuration loading
    This commit updates the code responsible for loading the Active Storage service configuration. It now checks for the existence of a configuration file specific to the current Rails environment. If the environment-specific file is not found, it falls back to the general configuration file. This change improves the flexibility and reliability of the service configuration loading process.

--- lib/storage_tables/engine.rb
+++ lib/storage_tables/engine.rb
@@ -36,7 +36,15 @@ module StorageTables
     initializer "storage_tables.services" do
       ActiveSupport.on_load(:storage_tables_blob) do
         # Use the application's configured Active Storage service.
-        configs = Rails.configuration.active_storage.service_configurations
+        configs = Rails.configuration.active_storage.service_configurations ||=
+          begin
+            config_file = Rails.root.join("config/storage/#{Rails.env}.yml")
+            config_file = Rails.root.join("config/storage.yml") unless config_file.exist?
+            raise("Couldn't find Active Storage configuration in #{config_file}") unless config_file.exist?
+
+            ActiveSupport::ConfigurationFile.parse(config_file)
+          end
+
         StorageTables::Blob.services = StorageTables::Service::Registry.new(configs)
 
         config_choice = Rails.configuration.storage_tables.service
