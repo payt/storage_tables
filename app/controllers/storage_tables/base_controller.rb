fd36a48700f11033d7e155309deca5a69e674f08
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-09-19 11:09:54 +0200

    Refactor protect_from_forgery in base controller and disk controller

--- app/controllers/storage_tables/base_controller.rb
+++ app/controllers/storage_tables/base_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 module StorageTables
-    class BaseController < ActionController::Base
-        include StorageTables::SetCurrent
+  # Base controller for all controllers in the storage_tables engine.
+  class BaseController < ApplicationController
+    include StorageTables::SetCurrent
 
-        protect_from_forgery with: :exception
-      
-        self.etag_with_template_digest = false
-    end
+    protect_from_forgery with: :exception
+
+    self.etag_with_template_digest = false
+  end
 end
