3b58ad2226cb57d466c899adcada33349b258235
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2025-06-06 11:30:59 +0200

    feat: add content type configuration for binary and inline file serving

--- lib/storage_tables/engine.rb
+++ lib/storage_tables/engine.rb
@@ -15,6 +15,31 @@ module StorageTables
     config.storage_tables = ActiveSupport::OrderedOptions.new
     config.storage_tables.queues = ActiveSupport::InheritableOptions.new
 
+    config.storage_tables.content_types_to_serve_as_binary = [
+      "text/html",
+      "image/svg+xml",
+      "application/postscript",
+      "application/x-shockwave-flash",
+      "text/xml",
+      "application/xml",
+      "application/xhtml+xml",
+      "application/mathml+xml",
+      "text/cache-manifest"
+    ]
+
+    config.storage_tables.content_types_allowed_inline = [
+      "image/webp",
+      "image/avif",
+      "image/png",
+      "image/gif",
+      "image/jpeg",
+      "image/tiff",
+      "image/bmp",
+      "image/vnd.adobe.photoshop",
+      "image/vnd.microsoft.icon",
+      "application/pdf"
+    ]
+
     initializer "storage_tables.attached" do
       require "storage_tables/attached"
 
@@ -32,6 +57,10 @@ module StorageTables
     initializer "storage_tables.configs" do
       config.after_initialize do |app|
         StorageTables.routes_prefix = app.config.storage_tables.routes_prefix || "/rails/storage_tables"
+
+        StorageTables.binary_content_type = app.config.storage_tables.binary_content_type || "application/octet-stream"
+        StorageTables.content_types_to_serve_as_binary = app.config.storage_tables.content_types_to_serve_as_binary || []
+        StorageTables.content_types_allowed_inline = app.config.storage_tables.content_types_allowed_inline || []
       end
     end
 
