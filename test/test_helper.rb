d880f5ea6fbc964d4e7367270ff9416f3d190f47
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2025-06-20 13:50:18 +0200

    fix: prevent test data leaks by properly cleaning up attachments before blobs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -39,7 +39,10 @@ module ActiveSupport
 
     teardown do
       StorageTables::Current.reset
-      StorageTables::Blob.delete_all
+      StorageTables::UserAvatarAttachment.find_each(&:delete)
+      StorageTables::UserPhotoAttachment.find_each(&:delete)
+      StorageTables::Blob.update_all(attachments_count: 0) # rubocop:disable Rails/SkipsModelValidations
+      StorageTables::Blob.find_each(&:delete)
     end
 
     private
