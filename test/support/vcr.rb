e79ac81754589de57e8304a6f5e6495c123b05ae
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2026-03-04 13:43:28 +0000

    chore: update VCR configuration to remove ignored host
    - Remove the ignored host "169.254.169.254" from VCR configuration for cleaner setup.

--- test/support/vcr.rb
+++ test/support/vcr.rb
@@ -5,7 +5,6 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "test/fixtures/cassettes"
   config.hook_into :webmock
-  config.ignore_hosts "169.254.169.254"
 
   config.default_cassette_options = { match_requests_on: [:method,
                                                           VCR.request_matchers.uri_without_param("X-Amz-Date",
